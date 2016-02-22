e4cafa6117c9f53ad6b9a78889246e9f4494856d
Rohan Taneja <rohantaneja63@gmail.com>
2016-02-22 20:23:53 +0530

    Enable temasek unofficial build tag

--- cm.mk
+++ cm.mk
@@ -8,6 +8,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/mediatek/mt6582/device_mt6582.mk)
+
+# Custom unofficial build tag
+TARGET_UNOFFICIAL_BUILD_ID := temasek
  
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mt6582
