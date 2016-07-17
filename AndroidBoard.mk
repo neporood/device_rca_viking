5031f30e4cd486eea00ef90debb58fe09d60016d
Nepo Rood <neporood@users.noreply.github.com>
2016-07-17 11:09:47 -0500

    Update to build Omni

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/Alco/RCT6303W87DK/AndroidBoardVendor.mk
+-include vendor/rca/viking/AndroidBoardVendor.mk
